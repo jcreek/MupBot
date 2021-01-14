6ed339ca26eca297aa903883aabffecf23d16d4f
Josh Creek <jcreek@hathorsoft.com>
2021-01-14 17:48:48 +0000

    feat(*): Add commands library

---
+++ commands.js
@@ -0,0 +1,20 @@
+module.exports = {
+  matchCommand: function (Discord, config, logger, message, command, args) {
+    switch(command) {
+      case 'help':
+        commandHelp(Discord, config, logger, message, command, args);
+        break;
+      default:
+        message.channel.send(`That command is not one I know yet - but you could add it! To find out more visit ${config.github_url}`);
+    }
+  },
+};
+
+function commandHelp (Discord, config, logger, message, command, args) {
+  const msg = `
+The commands available to you are:
+- ${config.prefix}commandgoeshere   - Do something
+  `;
+
+  message.channel.send(msg);
+}
