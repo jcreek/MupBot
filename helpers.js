245903b6207be1357b2b6600ec3633580ec10323
Olly Nicholass <ollynicholassofficial@gmail.com>
2021-01-15 11:41:35 +0000

    feat(*): Change embed helper to return for more flexibilty

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // Return a stylised embed for consistancy - this allows you to add additional properties like fields
   sendEmbedMessage: function (Discord, logger, message, title, description, colour = '#F4B400') {
       // For documentation see https://github.com/AnIdiotsGuide/discordjs-bot-guide/blob/master/first-bot/using-embeds-in-messages.md
       try {
@@ -9,8 +10,7 @@ module.exports = {
           .setFooter('This is a community-made open source bot - if it goes down, fix it')
           .setThumbnail('https://cdn.discordapp.com/icons/333376110329069578/b5cf0e90b74a3e1b828c15a262b7a5c8.png?size=128')
           .setTimestamp();
-
-        message.channel.send(embed);
+        return embed;
       } catch (error) {
         logger.error('Failed to send embed message', error);
         message.channel.send('There was a problem, sorry!');
